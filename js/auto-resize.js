38c92afe32e58db4005e3eecbe466715e1971a94
DuyLQ <luuquangduy291@gmail.com>
2025-05-21 11:14:06 +0700

    Delete js directory

--- js/auto-resize.js
+++
@@ -1,7 +0,0 @@
-// js/auto-resize.js
-function sendHeightToParent() {
-  const height = document.body.scrollHeight;
-  parent.postMessage({ type: "setIframeHeight", height: height }, "https://blog.marshome.io.vn");
-}
-window.addEventListener("load", sendHeightToParent);
-window.addEventListener("resize", sendHeightToParent);
